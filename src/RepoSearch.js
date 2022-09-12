9e0410e0f1425cbfd6c38723069ca5dfe80ed126
João Seda <joao_ps@dcc.ufrj.br>
2022-09-12 10:12:10 -0300

    Correções de problemas com o último commit

--- src/RepoSearch.js
+++ src/RepoSearch.js
@@ -39,4 +39,54 @@ function RepoSearch() {
     }
 
     const handleLastPage = () => {
-        setPage(page
+        setPage(page-1);
+    }
+
+    
+    return (
+        <CContainer className='p-3'>
+            <CRow className='my-3 align-items-start'>
+                    <CCol md={6}>
+                        <CFormInput type="text" onChange={(e) => setQuery(e.target.value)} />
+                    </CCol>
+                    <CCol md={1}>
+                        <CButton type="button" color="secondary" style={{color:"white"}} onClick={() => {setPage(1); searchRepos();}}>Pesquisar</CButton>
+                    </CCol>
+                    <CCol>
+                        Itens por página:
+                        <CFormSelect className='d-inline mx-2' id='page-limit' value={page_limit} style={{width: "25%"}} onChange={(e) => setPageLimit(e.target.value)}
+                        options={[
+                            { label: "5", value: 5},
+                            { label: "10", value: 10},
+                            { label: "15", value: 15},
+                            { label: "20", value: 20}
+                        ]} />
+                    </CCol>
+                    <CCol md={2} className='ms-auto'>
+                    {
+                    page_count > 1 ?
+                        <CPagination>
+                            { page > 1 ? <CPaginationItem onClick={handleLastPage}> <FaAngleDoubleLeft/> </CPaginationItem> : "" }
+                            { page > 1 ? <CPaginationItem> {page-1} </CPaginationItem> : "" }
+                            <CPaginationItem active> {page} </CPaginationItem>
+                            { page < page_count ? <CPaginationItem> {page+1} </CPaginationItem> : "" }
+                            { page < page_count ? <CPaginationItem onClick={handleNextPage}> <FaAngleDoubleRight/> </CPaginationItem> : "" }
+                        </CPagination>
+                    :
+                        ""
+                    }
+                    </CCol>
+            </CRow>
+            {
+                loading ?
+                    <Vortex color="#2f2f2f" />
+                :
+                    <CRow>
+                        {repos ? repos.map((repo) => <RepoCard key={repo.id} repo={repo} />) : ""}
+                    </CRow>
+            }
+        </CContainer>
+    )
+}
+
+export default RepoSearch;
